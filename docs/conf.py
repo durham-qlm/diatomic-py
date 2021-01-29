b9ba182e5fc0ee37f5e1f870d65d0b9eba57d429
Jake Blackmore <jake.blackmore820@gmail.com>
2021-01-29 19:22:17 +0000

    more autodocs faff

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ release = '1.1.0'
 extensions = ['sphinx.ext.napoleon'
 ]
 
+autodoc_mock_imports = ['pyprind','numpy','matplotlib','sympy','scipy']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -52,4 +53,4 @@ html_theme = 'nature'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []

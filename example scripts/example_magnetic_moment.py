699bcde083df680e679886ee77eda31dccccaf76
Philip Gregory <106588980+PhilipDGregory@users.noreply.github.com>
2022-06-01 15:09:53 +0100

    Update and rename example_magnetic_moment.py to example scripts/example_magnetic_moment.py

--- example scripts/example_magnetic_moment.py
+++ example scripts/example_magnetic_moment.py
@@ -1,9 +1,9 @@
 import numpy
 import matplotlib.pyplot as pyplot
-import hamiltonian
-from constants import Rb87Cs133
+import diatom.hamiltonian
+from diatom.constants import Rb87Cs133
 from numpy.linalg import eigh
-import calculate
+import diatom.calculate
 import scipy.constants 
 
 muN = scipy.constants.physical_constants['nuclear magneton'][0]

90e50c06fc1d72807e5b928d81936104ea547fd8
Philip Gregory <106588980+PhilipDGregory@users.noreply.github.com>
2022-06-06 12:54:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import Cython.Build import cythonize
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -13,6 +14,27 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/PhilipDGregory/Diatomic-Py",
     packages= ['diatom'],
+    license = 'Boost Software License - Version 1.0',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['numpy>=1.19','sympy>=1.4','scipy>=1.1'],
+    python_requires='>=3.7',
+)
+
+setuptools.setup(
+    name="diatomc",
+    version="1.2.0",
+    author="Jake Blackmore",
+    author_email="j.a.blackmore@durham.ac.uk",
+    description="A package for calculating rotational and hyperfine structure of singlet diatomic molecules.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PhilipDGregory/Diatomic-Py",
+    ext_modules=cythonize("calculate.py"),
+    zip_safe=False,
     license = 'Boost Software License - Version 1.0',
     classifiers=[
         "Programming Language :: Python :: 3",
